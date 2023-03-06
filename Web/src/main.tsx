543d337e89bd3da32aeeaaf7b80f785ccb1ce573
Sahil Yeole <sahilyeole93@gmail.com>
2023-03-07 02:44:27 +0530

    update main.tsx

--- Web/src/main.tsx
+++ Web/src/main.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { Provider } from 'react-redux';
-import {store} from './app/store';
-import {ApolloClient,InMemoryCache,ApolloProvider} from '@apollo/client'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const client = new ApolloClient({
-    uri: import.meta.env.VITE_APOLLO_URI,
-    // uri: 'http://localhost:4000/graphql',
-    cache: new InMemoryCache()
-})
+  uri: import.meta.env.VITE_APOLLO_URI,
+  cache: new InMemoryCache(),
+});
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <ApolloProvider client={client}>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ApolloProvider client={client}>
     <Provider store={store}>
-        <GoogleOAuthProvider clientId="675091325788-79tn0tlaoj9oeuoprlghf2tfvntbggju.apps.googleusercontent.com">
-    <App />
-    </GoogleOAuthProvider>
+      <GoogleOAuthProvider clientId="675091325788-79tn0tlaoj9oeuoprlghf2tfvntbggju.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
     </Provider>
-    </ApolloProvider>
-)
+  </ApolloProvider>
+);
