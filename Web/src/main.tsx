112cec13bfca5adee8a2935de4b89082a128f9b3
Sahil Yeole <sahilyeole93@gmail.com>
2023-03-07 12:44:39 +0530

    test deploy

--- Web/src/main.tsx
+++ Web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
@@ -10,16 +10,17 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const client = new ApolloClient({
-  uri: process.env.VITE_APOLLO_URI,
+  // uri: process.env.VITE_APOLLO_URI,
+  uri: import.meta.env.VITE_APOLLO_URI,
   cache: new InMemoryCache(),
 });
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ApolloProvider client={client}>
     <Provider store={store}>
-      <GoogleOAuthProvider clientId="675091325788-79tn0tlaoj9oeuoprlghf2tfvntbggju.apps.googleusercontent.com">
+      {/* <GoogleOAuthProvider clientId="675091325788-79tn0tlaoj9oeuoprlghf2tfvntbggju.apps.googleusercontent.com"> */}
         <App />
-      </GoogleOAuthProvider>
+      {/* </GoogleOAuthProvider> */}
     </Provider>
   </ApolloProvider>
 );
