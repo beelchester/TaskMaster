e906f8c57797b652a99c63b2cd03f0a8272b5a64
Sahil Yeole <sahilyeole93@gmail.com>
2023-03-07 21:15:40 +0530

    remove build rollup option from vite config

--- Web/vite.config.ts
+++ Web/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['graphql']
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['graphql']
+  //   }
+  // }
 })
