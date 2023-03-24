7937a8210b8d0cdf2341d27eccd7f5f8c25bd0f6
Sahil Yeole <sahilyeole93@gmail.com>
2023-03-24 21:31:17 +0530

    add delete project

--- Mobile/src/modal/AddProject.js
+++ Mobile/src/modal/AddProject.js
@@ -2,6 +2,7 @@ import {
   Text,
   TouchableOpacity,
   Dimensions,
+    View
 } from "react-native";
 import { Overlay, Input } from "@rneui/themed";
 import React, { useState,  useEffect } from "react";
@@ -18,9 +19,10 @@ import { initialTasks } from "../features/taskSlice";
 import { fetchProject } from "../features/projectSlice";
 import DropDownPicker from "react-native-dropdown-picker";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import { CREATE_PROJECT, UPDATE_PROJECT } from "../graphql/ProjectMutations";
+import { CREATE_PROJECT, DELETE_PROJECT, UPDATE_PROJECT } from "../graphql/ProjectMutations";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useWindowDimensions } from "react-native";
+import { Icon } from "@rneui/base";
 
 const AddProject = ({showAddProject,setShowAddProject,currentProject,mode}) => {
 
@@ -64,6 +66,43 @@ const handleCreateProject = (name) => {
       ],
     })
   };
+    const user = useQuery(GET_USER, {
+        variables: { email: "sahilyeole93@gmail.com" },
+    });
+    const dispatch = useDispatch();
+    const fetchUser = () => {
+        if (user.loading) {
+            dispatch(fetchUserStart());
+        }
+        if (user.error) {
+            dispatch(fetchUserFailure(user.error));
+            return user.error;
+        }
+        if (user.data) {
+            dispatch(fetchUserSuccess(user.data.getUser.user));
+            // console.log(user.data.getUser.user)
+            dispatch(fetchProject(user.data.getUser.user.projects));
+            dispatch(initialTasks(user.data.getTasks));
+            // console.log(user.data.getTasks)
+            //   console.log(user.data.getUser.accessToken)
+            // localStorage.setItem("accessToken", user.data.getUser.accessToken);
+            // localStorage.setItem("refreshToken", user.data.getUser.refreshToken);
+        }
+    };
+    const [deleteProject] = useMutation(DELETE_PROJECT);
+    const handleDeleteProject = (name) => {
+            deleteProject({
+      variables: {
+        projectName: name,
+        email: "sahilyeole93@gmail.com" 
+      },
+      refetchQueries: [
+        { query: GET_USER, variables: { email: "sahilyeole93@gmail.com" } },
+      ],
+    })
+
+    }
+
 function submitProject() {
     if (mode === "edit") {
       handleEditProject(toEdit, projectName);
@@ -74,6 +113,13 @@ function submitProject() {
     handleCreateProject(projectName);
     setShowAddProject(false);
     setProjectName("");
+  }
+
+function deleteProjectHandler() {
+    handleDeleteProject(toEdit);
+    setShowAddProject(false);
+    setProjectName("");
+    fetchUser();
   }
 
   
@@ -106,19 +152,29 @@ function submitProject() {
     {mode === "edit" ? "Edit Project" : "Add Project"}
     </Text>
 
-    <Input 
-    
-    value={
-      projectName
-    } onChangeText={(text) => setProjectName(text)}
-  placeholder="Project Name" style={{ color: "white" }} />
-
+    <Input value={ projectName } onChangeText={(text) => setProjectName(text)} placeholder="Project Name" style={{ color: "white" }} />
+      <View style={{
+          flexDirection:'row',
+            alignItems:'center',
+              justifyContent:'space-between',
+      }}>
 <TouchableOpacity onPress={submitProject}  style={{backgroundColor:"#33c6dd", width:80,
 height:40, borderRadius:5, justifyContent:'center', alignItems:'center', marginTop:15
 }}  activeOpacity={0.6} >
 <Text style={{color:'black',fontWeight:'bold',}}> {mode =="edit" ? "Edit" : "Add"}
 </Text>
     </TouchableOpacity>
+
+    {
+    mode === "edit" &&
+        <TouchableOpacity onPress={deleteProjectHandler} >
+    <Icon name="delete" size={30} color="white" style={{
+        marginTop:13,
+            marginRight:10
+    }}/>
+    </TouchableOpacity>
+    }
+</View>
   </Overlay>
   )
 }
